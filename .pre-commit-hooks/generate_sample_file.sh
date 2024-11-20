76b714a3a1db07a04b136db0ed0c64500c5a98f5
evans.jiang <evans.jiang@gmail.com>
2024-11-20 14:58:56 +0800

    归档

--- .pre-commit-hooks/generate_sample_file.sh
+++ .pre-commit-hooks/generate_sample_file.sh
@@ -2,8 +2,8 @@
 # 执行出错时，退出
 set -e
 
-# check use
-if [ $# -ne 2 ];then
+# check use. 参数个数小于2，提示使用方法
+if [ $# -lt 2 ];then
 	echo "Usage: $0 <filename> <file_type>"
     	exit 1
 fi
@@ -17,7 +17,8 @@ fi
 
 # generate sample file
 config_file_name="$1"
-config_file_name_sample="${config_file_name}.sample"
+config_file_name_sample_target="${config_file_name}.sample"
+config_file_name_sample="${config_file_name}.temp"
 cp ${config_file_name} ${config_file_name_sample}
 
 # read file
@@ -54,9 +55,40 @@ echo  "${all_key_value}" | while IFS= read -r line; do
 
 	fi
 	# 替换
-	#if echo "$line" | grep -q '#'; then
-    	#	echo "字符串中包含#字符" $line
-	#fi
-	sed -i "s#${line}#${key_value_new}#g" ${config_file_name_sample}
+	# 使用 uname 命令获取系统类型
+	os_type=$(uname)
+
+	# 判断系统类型并输出
+	if [ "$os_type" == "Darwin" ]; then
+		sed -i "" "s#${line}#${key_value_new}#g" "${config_file_name_sample}"
+	elif [ "$os_type" == "Linux" ]; then
+		sed -i "s#${line}#${key_value_new}#g" "${config_file_name_sample}"
+	elif [ "$os_type" == "FreeBSD" ]; then
+		echo "This is FreeBSD."
+		exit 1
+	else
+		echo "Unknown operating system."
+		exit 1
+	fi
+
 done
 echo "${config_file_name_sample} handle ok!"
+# check file exist
+if [ ! -f "${config_file_name_sample_target}" ];then
+	mv ${config_file_name_sample} ${config_file_name_sample_target}
+else
+	# 计算文件的 MD5 校验和
+	md5_1=$(md5sum "${config_file_name_sample_target}" | awk '{print $1}')
+	md5_2=$(md5sum "${config_file_name_sample}" | awk '{print $1}')
+
+	# 判断 MD5 校验和是否相同.
+	# TODO 根据系统，mac系统使用md5,linux系统使用md5sum
+	# TODO 生成env文件和 toml文件拆分为2个hook
+	if [ "$md5_1" = "$md5_2" ]; then
+		echo "MD5 checksums match. The files are identical."
+		rm -rf ${config_file_name_sample}
+	else
+		echo "MD5 checksums do not match. The files are different."
+		mv ${config_file_name_sample} ${config_file_name_sample_target}
+	fi
+fi
--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 > 在本地.pre-commit-config.yaml 文件中追加下面配置，并修改。
 > 注意不要复制`repos:`
 
-#### check-coverage
+#### id: check-coverage
 在指定容器中运行coverage检查django的项目的单元测试覆盖率。
 ```yaml
 repos:
@@ -22,11 +22,24 @@ repos:
       language: script
       stages: [commit]
 ```
-#### generate_sample_file
+#### id: generate_sample_file
 实现在提交git commit时,
 1. 从本地 `xxx.toml` 文件生成 `xxx.toml.sample`文件
 2. 从本地 `.env` 文件生成 `.env.sample`文件
 
+### id: 使用容器中的mypy进行检查
+#TODO doing
+1. 默认提供的mypy,是在一个独立的环境中，需要添加由于缺少一些依赖项，所以检查效果与本地开发环境中的行为不一致。
+2. pre-commit的版本和本地开发的版本不同步，造成混乱
+```yaml
+ - repo: https://github.com/pre-commit/mirrors-mypy
+    rev: v1.9.0
+    hooks:
+      - id: mypy
+        args: [--config-file, language_backend/pyproject.toml]
+        additional_dependencies: [django==4.2]
+```
+经过修改之后，当前每次提交前的检查，是使用的本地开发容器中的mypy进行检查
 
 ### 如果没有安装pre-commit:
 
